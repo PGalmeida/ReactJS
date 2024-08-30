1878ea264d4c5ea6b37e2c7a4e867744e3b70085
PGAlmeida <pedro8tp@gmail.com>
2024-08-30 10:52:22 -0300

    Atribuição dos componentes

---
+++ crud_livro/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import LivroList from '../components/LivroList';
+
+function Home() {
+    return (
+        <div>
+            <h1>gerenciamento de livros</h1>
+            <LivroList />
+        </div>
+    );
+}
+
+export default Home;
