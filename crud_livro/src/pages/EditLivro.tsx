5fd22edb77c62d2ff05f0bb7c4aecbf76592b6a3
PGAlmeida <pedro8tp@gmail.com>
2024-08-30 10:52:02 -0300

    Editar um Livro

---
+++ crud_livro/src/pages/EditLivro.tsx
@@ -0,0 +1,11 @@
+import LivroForm from '../components/LivroForm';
+
+function EditLivro() {
+    return (
+        <div>
+            <h1>Editar livro</h1>
+            <LivroForm />
+        </div>
+    );
+}
+export default EditLivro;
